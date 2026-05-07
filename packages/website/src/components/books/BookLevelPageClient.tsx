34c196b6b82064460f0fe5cdf5fcf8e9f7fc9a9a
Bourne <xiaopei0206@icloud.com>
2026-05-07 17:42:53 +0800

    fix the column in book list

--- packages/website/src/components/books/BookLevelPageClient.tsx
+++ packages/website/src/components/books/BookLevelPageClient.tsx
@@ -267,7 +267,7 @@ export function BookLevelPageClient({ levelData, levels }: { levelData: LevelDat
               })}
             </div>
           </div>
-        <section className="mt-8 grid grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-4">
+        <section className="mt-8 grid gap-4 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {levelData.books.map((book) => (
             <div
               key={book.slug}
